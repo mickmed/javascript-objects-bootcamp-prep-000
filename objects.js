22f52f7c44be163ca11bf230c472c3f4d3da01a0
Michael Roth <mick2090@gmail.com>
2018-03-05 21:29:37 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -7,8 +7,7 @@ function updatePlaylist(playlist, artistName, songTitle){
 }
 
 
-function updatePlaylist(playlist, artistName, songTitle){
-  
+function removeFromPlaylist(playlist, artistName)
   playlist[artistName] = songTitle;
   return playlist;
 }
