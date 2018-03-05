4a3bb3a5dae94dd0cec524c1aefd7b2e14d7c9fd
Michael Roth <mick2090@gmail.com>
2018-03-05 21:40:52 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -7,7 +7,7 @@ function updatePlaylist(playlist, artistName, songTitle){
 }
 
 
-function removeFromPlaylist(playlist, artistName){
+function removeFromPlaylist(songTitle, artistName){
   delete playlist.artistName;
   return playlist;
 }
